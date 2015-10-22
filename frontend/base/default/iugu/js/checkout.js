d869841bdbc5a7e1ed4cdb024e67d84a7af29abf
Bruno Ideguchi <bmideguchi@gmail.com>
2015-10-22 01:09:52 -0200

    coisas do iugu

---
+++ frontend/base/default/iugu/js/checkout.js
@@ -0,0 +1,50 @@
+/**
+ * @category    Inovarti
+ * @package     Inovarti_Iugu
+ * @copyright   Copyright (c) 2014 Inovarti. (http://www.inovarti.com.br)
+ */
+
+// Default Magento Checkout
+Payment.prototype._save = Payment.prototype.save;
+Payment.prototype.save = function() {
+    if (checkout.loadWaiting!=false) return;
+    var validator = new Validation(this.form);
+    if (this.validate() && validator.validate()) {
+        if (this.currentMethod == 'iugu_cc') {
+            var $installments = $(payment.currentMethod+'_installments');
+            if ($installments.selectedIndex > 0) {
+                $(payment.currentMethod+'_installment_description').value = $installments.options[$installments.selectedIndex].text;
+            } else {
+                $(payment.currentMethod+'_installment_description').value = '';
+            }
+            this.iugu_cc_data = {};
+            var fields = ['installments', 'installment_description', 'cc_type', 'cc_number', 'cc_owner', 'expiration', 'expiration_yr', 'cc_cid'];
+            fields.each(function(field){
+                this.iugu_cc_data[field] = $(this.currentMethod+'_'+field).value;
+            }.bind(this));
+        } else {
+            this.iugu_cc_data = null; //clear data
+        }
+        this._save();
+    }
+};
+
+Review.prototype._save = Review.prototype.save;
+Review.prototype.save = function() {
+    var skipToken = $(payment.currentMethod+'_iugu_customer_payment_method_id')
+        && $(payment.currentMethod+'_iugu_customer_payment_method_id').value != "";
+    if (payment.currentMethod == 'iugu_cc' && !skipToken) {
+        checkout.setLoadWaiting('review');
+        Iugu.createPaymentToken($(payment.form), function(data) {
+            checkout.setLoadWaiting(false);
+            if (data.errors) {
+                alert(JSON.stringify(data.errors));
+            } else {
+                $(payment.currentMethod+'_iugu_token').value = data.id;
+                this._save();
+            }
+        }.bind(this));
+    } else {
+        this._save();
+    }
+};
